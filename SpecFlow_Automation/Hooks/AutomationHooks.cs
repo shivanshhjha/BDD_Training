f5c29d371066d545fee927238453b25b76b2ecb0
Shivansh Jha <60254797+shivanshhjha@users.noreply.github.com>
2022-09-13 11:39:46 +0530

    Hooks updated

--- SpecFlow_Automation/Hooks/AutomationHooks.cs
+++ SpecFlow_Automation/Hooks/AutomationHooks.cs
@@ -4,22 +4,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
 
 namespace SpecFlow_Automation.Hooks
 {
+
     [Binding]
     public class AutomationHooks
     {
         public static IWebDriver driver;
 
-        //[AfterScenario]
-        //public void EndScenario()
-        //{
-        //    if (driver != null)
-        //    {
-        //        driver.Quit();
-        //    }
-        //}
+        [AfterScenario]
+        public void EndScenario()
+        {
+            if (driver != null)
+            {
+                driver.Quit();
+            }
+        }
     }
 
 
